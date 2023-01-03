69b9ce180ac5bdbbc932ae4d8b02d9266e98f04f
N152-U <79331748+N152-U@users.noreply.github.com>
2023-01-03 11:25:55 -0600

    feat: first commit

---
+++ src/main/java/com/ansm/template/HomeController.java
@@ -0,0 +1,5 @@
+package com.ansm.template;
+
+public class HomeController {
+
+}
---
+++ src/test/java/com/ansm/template/TemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansm.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
