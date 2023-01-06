3bed0aa0593393e06d7e1a946e5af23e323548be
N152-U <79331748+N152-U@users.noreply.github.com>
2023-01-06 12:34:28 -0600

    feat: HomeController

--- src/main/java/com/ansm/template/HomeController.java
+++ src/main/java/com/ansm/template/HomeController.java
@@ -1,5 +1,14 @@
 package com.ansm.template;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
+	@GetMapping("/")
+	public String home() {
+		System.out.println("Ejecutando");
+		return "Home Page";
+	}
 }
